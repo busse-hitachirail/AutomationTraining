d517a8a2a873583d6a95c3e2fedf548931998fde
Narendranath Iyer <naren.iyer100@gmail.com>
2025-03-11 08:13:44 -0700

    First Commit

--- PythonTraining/dictionary_learn.py
+++ PythonTraining/dictionary_learn.py
@@ -1,3 +1,7 @@
+#A dictionary in Python is a collection of key-value pairs.
+# Each key is connected to a value, and you can use a
+# key to access the value associated with that key.
+
 employee_data = {'age': 25, 'state': 'CA'}
 print(employee_data['age'])
 print(employee_data['state'])
