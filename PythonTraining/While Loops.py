a268ca48f49cd80555562e12ed8572b05bf93718
Narendranath Iyer <naren.iyer100@gmail.com>
2025-03-24 13:36:19 -0700

    fixed Mistake

--- PythonTraining/While Loops.py
+++ PythonTraining/While Loops.py
@@ -1,6 +1,4 @@
 # Input
-message = input("Tell me something, and I will repeat it back to you: ") # Takes an input
-print(message)
 
 name = input("Please enter your name: ")
 print(f"\nHello, {name}!")
@@ -10,20 +8,23 @@ prompt += "\nWhat is your favorite project? "
 project = input(prompt)
 print(f"\n{name}, the {project} project!, met the scrum standards and 95% efficiency"
       f"for last three years!")
-
+print("________________________")
 
 #Using int() to Accept Numerical Input Fixing it by adding int
+#TypeError: '>=' not supported between instances of 'str' and 'int'
 years = input("How many years have you been at Hitachi?")
 years = int(years)
 if years >= 5:
-    print("What an experienced engineer")
+    print("What an experienced engineer!")
 else:
     print("Still have a lot to learn")
 
+print("________________________")
 # What is a Modulo Operator? Tells you what the remainder is
-x = 4 % 3
+x = 6 % 3
 print(x)
 
+print("________________________")
 # Simple Example of Even and Odd.
 number = input("Enter a number, and I'll tell you if it's even or odd: ")
 number = int(number)
@@ -32,14 +33,14 @@ if number % 2 == 0:
     print(f"\nThe number {number} is even.")
 else:
     print(f"\nThe number {number} is odd.")
-
+print("________________________")
 # Homework
 """
 Rental Car: Write a program that asks the user what kind of rental car they would like. 
 Print a message about that car, such as “Let me see if I can find you a Subaru.”
 """
 
-#The for loop takes a collection of items and executes a block of code once for each item in the collection.
+# The for loop takes a collection of items and executes a block of code once for each item in the collection.
 # In contrast, the while loop runs as long as, or while, a certain condition is true.
 
 # Sample Code
@@ -47,45 +48,23 @@ current_number = 1
 while current_number <= 5:
     print(current_number)
     current_number += 1 #(The += operator is shorthand for current_number = current_number + 1.)
-
-
+print("________________________")
 
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
-message = "" #If Python has nothing to compare, it won’t be able to continue running the program.
-# To solve this problem, we make sure to give message an initial value.
-while message != 'quit':
-    message = input(prompt)
-    print(message)
 
-
-# Not to print Quit
-prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' to end the program. "
 message = ""
-
 while message != 'quit':
     message = input(prompt)
+
     if message != 'quit':
         print(message)
 
-# Using a Flag
-prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' to end the program. "
-
-active = True # The variable (Flag) is set to True. The program starts in active state
-while active:
-    message = input(prompt)
-
-    if message == 'quit':
-        active = False
-    else:
-        print(message)
+print("________________________")
 
 # Using a break statement
 prompt = "\nPlease enter the name of managers you have reported to:"
 prompt += "\n(Enter 'quit' when you are finished.) "
-
 while True: # This loop will run forever!
     manager = input(prompt)
 
@@ -93,6 +72,7 @@ while True: # This loop will run forever!
         break
     else:
         print(f"I'd reported to  {manager.title()}!")
+print('________________________________')
 
 # Use Continue
 current_number = 0
@@ -101,14 +81,30 @@ while current_number < 10:
     if current_number % 2 == 0:
         continue
     print(current_number)
+print('________________________________')
 
 
 # Infinite Loop - Fix this Infinite Loop
-"""
-x = 1
+# This loop runs forever!
+"""x = 1
 while x <= 5:
-    print(x)
-"""
+    print(x) """
+
+# Setting a Flag
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+
+T
 
 # Homework
 """Movie Tickets: A movie theater charges different ticket prices depending on a person’s age. 
